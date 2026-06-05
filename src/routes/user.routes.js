01e1b21f48c169109dd0f8d92ea8fabf2a5ef6f4
Prakhartiwari-45 <prakhart240@gmail.com>
2026-06-05 13:02:21 +0530

    enhance user routes: add change password, current user, update account, and user profile retrieval endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import {registerUser,loginUser,  logOutUser ,refreshAccessToken} from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logOutUser ,
+    refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserProfile, getUserWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -19,13 +23,18 @@ router.route("/register").post(
     registerUser);
 // router.route("/login").post(loginUser)
 
-router.route("/login").post(
-    loginUser
-)
+router.route("/login").post(loginUser)
 
 //secured Routes
 
 router.route("/logOut").post(verifyJWT ,logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserProfile)
+router.route("/history").get(verifyJWT,getUserWatchHistory)
 
 export default router
